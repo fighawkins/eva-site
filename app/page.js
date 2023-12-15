d2f4376548bfe538e14cd35fe7fb084f55b053dc
Fig Hawkins <fighawkins@Figs-MacBook-Air.local>
2023-12-15 15:46:14 -0800

    initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '4rem' }}>Eva Knowles</h1>
+    </div>
+  );
+};
+
+export default Home;
