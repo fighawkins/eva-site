f06c97e2b9d2652dd2cc61bee7ce6a76ff549133
Fig Hawkins <fighawkins@Figs-MacBook-Air.local>
2023-12-16 16:59:36 -0800

    pages added

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import RandomRotateText from "./RandomRotateText";
 
 const Home = () => (
   <div className="flex justify-center items-center text-center">
-    <div className="w-3/5 lg:w-1/3 h-3/5    relative opacity-80">
+    <div className="w-3/5 lg:w-1/3 h-3/5    relative ">
       <img
         src="/image3.png"
         alt="Image"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover opacity-80"
       />
-      <h1 className="absolute top-0  transform  -translate-y-10 lg:pl-8 text-2xl text-center pl-12 lg:pl-24 lg:text-7xl lg:-translate-y-0 text-black lg:pt-12">
+      <h1 className="absolute top-0  transform  -translate-y-10 lg:pl-8 text-2xl text-center pl-12  lg:text-7xl lg:-translate-y-0 text-black lg:pt-12">
         <RandomRotateText text="Eva Knowles" />
       </h1>
       <div className="absolute bottom-0 w-full pb-4 text-white text-base lg:text-lg">
